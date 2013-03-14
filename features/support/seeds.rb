1dddcb7184429b4a8f082986e903cfbbe5bb6647
Kevin Sánchez <kedavidsa@gmail.com>
2013-03-14 09:32:50 -0500

    Agregar entregable

--- features/support/seeds.rb
+++ features/support/seeds.rb
@@ -12,6 +12,7 @@ AfterConfiguration do
   #Profesore.create(:id=>1,:PRIMER_APELLIDO=>"apellido1", :PRIMER_NOMBRE=>"nombre1", :SEGUNDO_APELLIDO=>"apellido2", :SEGUNDO_NOMBRE=>"nombre2")
   #Estudiante.create(:CODIGO=>"1234", :id=>1, :PRIMER_APELLIDO=>"apellido1", :PRIMER_NOMBRE=>"nombre1", :SEGUNDO_APELLIDO=>"apellido2", :SEGUNDO_NOMBRE=>"nombre2")
   #Tesi.create(:DESCRIPCION=>"", :FECHA_INICIO=>Date.new(2001,2,3), :id=>1, :NOMBRE=>"tesis1", :PROFESOR_ID=>1)
+  #Entregable.create(:FECHA_ENTREGA=>Date.new(2013,2,2), :NOMBRE=>"Entrega",:id=>1, :TESIS_ID=>1, :URI_DOCUMENTO=>"https://www.dropbox.com/s/1ia8e7y0jcebatk/PaperFor")
   #TesisEstudiante.create(:ESTUDIANTE_ID => 1, :TESIS_ID => 1)
   #FuenteBibliografica.create( :AUTOR=>"autor1", :ESTUDIANTES_ID=>1, :FECHA_BIBLIOGRAFIA=>Date.new(2001,2,3), :FECHA_LECTURA=>Date.new(2001,2,3), :id=>1, :LIBRO=>"libro1", :RESUMEN=>"resumen del libro 1", :TESIS_ID=>1, :URL=>"www.libro1.com")
   #Compromiso.create(:FECHA_CIERRE=>Date.new(2021,2,3), :FECHA_COMPROMETIDA=>Date.new(2014,2,3), :FECHA_CREACION=>Date.new(2001,2,3), :id=>1, :ROL=>"profesor", :TESIS_ID=>1, :TITULO=>"compromiso1", :CONTENIDO=>"contenido del compromiso 1")
